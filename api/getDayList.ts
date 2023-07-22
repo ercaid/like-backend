3b393151deef82452686f7433332f942180c985b
ercai <ercaid@foxmail.com>
2023-07-22 11:36:51 +0800

    feat: 跨域

--- api/getDayList.ts
+++ api/getDayList.ts
@@ -3,6 +3,9 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { client } from "../utils/mongodb";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   const db = await client.db("like");
   var result = await db.collection("days").find().toArray();
   console.log(client);
