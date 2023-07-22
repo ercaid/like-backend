d736029b6336dd7afca9359f0d7b3fc5ebcb9f3d
ercai <ercaid@foxmail.com>
2023-07-22 11:17:40 +0800

    feat: db

--- api/getDayList.ts
+++ api/getDayList.ts
@@ -1,9 +1,12 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-import { db } from "../utils/mongodb";
+import { client } from "../utils/mongodb";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  const db = await client.db("like");
   var result = await db.collection("days").find().toArray();
-
+  console.log(client);
+  console.log(db);
+  console.log(result);
   res.status(200).json(result);
 };
