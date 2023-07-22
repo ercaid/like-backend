66e708dc2562758ee3edc7920b37bcda1339f677
ercai <ercaid@foxmail.com>
2023-07-22 12:49:56 +0800

    feat: 预检

--- api/editDay.ts
+++ api/editDay.ts
@@ -17,6 +17,10 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
   const body = req.body;
 
