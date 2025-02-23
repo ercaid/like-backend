d8c11dbd5bc3ed6e1ccf20f6c0602ba7d18bb689
ercaid <ercaid@foxmail.com>
2025-02-23 19:30:47 +0800

    feat: 连接

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const CONNECTION_STRING =
-  "mongodb+srv://ercaid:cad19971127@cluster0.0rntcum.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://chenandongcad:tv5iOiD3ymRvA2Ea@cluster0.a5aj2.mongodb.net";
 
 export const client = new MongoClient(CONNECTION_STRING, {
   serverApi: {
