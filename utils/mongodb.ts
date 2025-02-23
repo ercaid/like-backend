ecb199c2bcd9fd9f8d19d629f25e49d14e8fd5d5
ercaid <ercaid@foxmail.com>
2025-02-23 19:31:13 +0800

    feat: 连接

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -3,10 +3,4 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const CONNECTION_STRING =
   "mongodb+srv://chenandongcad:tv5iOiD3ymRvA2Ea@cluster0.a5aj2.mongodb.net";
 
-export const client = new MongoClient(CONNECTION_STRING, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+export const client = new MongoClient(CONNECTION_STRING);
