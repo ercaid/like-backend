566e8a3a3ec0a4d89ccebdc5e0aeb8b023e591ee
ercaid <ercaid@foxmail.com>
2025-02-23 18:50:35 +0800

    feat: 数据库连接

--- api/postData.ts
+++ api/postData.ts
@@ -11,21 +11,17 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
 
   const body = req.body;
 
-  // try {
-  //   const db = await client.db("xiaomei");
-  //   await db.collection("result").insertOne(body);
-  //   res.status(200).json({
-  //     code: 0,
-  //     data: "成功",
-  //   });
-  // } catch (e) {
-  //   res.status(200).json({
-  //     code: 1,
-  //     data: e,
-  //   });
-  // }
-  res.status(200).json({
-    code: 0,
-    data: "成功",
-  });
+  try {
+    const db = await client.db("xiaomei");
+    await db.collection("result").insertOne(body);
+    res.status(200).json({
+      code: 0,
+      data: "成功",
+    });
+  } catch (e) {
+    res.status(200).json({
+      code: 1,
+      data: e,
+    });
+  }
 };
